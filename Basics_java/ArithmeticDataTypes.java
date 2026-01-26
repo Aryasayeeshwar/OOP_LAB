50e2f9a4e8b7cf391a2f1ae363fc0e3a1f76cbab
Aryasayeeshwar <gudipalliaryasayeeshwar@gmail.com>
2026-01-26 20:19:50 +0530

    Add files via upload

---
+++ Basics_java/ArithmeticDataTypes.java
@@ -0,0 +1,11 @@
+class ArithmeticDataTypes {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 2.5f;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+}
